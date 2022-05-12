40269000120b0e43ee1ec1891d7bf23d61d83ba1
DominicChristianLaroya <98566084+DominicChristianLaroya@users.noreply.github.com>
2022-05-12 12:12:11 +0800

    Add files via upload

---
+++ FINALS/Script15.js
@@ -0,0 +1,20 @@
+var myName="Dominic Laroya"
+var myAge="19"
+var myPhoneNumber="09451489691"
+var myEmail="20209220@s.ubaguio.edu"
+var myAddress="Tavora Proper, PGLU"
+var myProgram="BSIT"
+var myID="20209220"
+var myYear="2nd yr"
+var mySection="IAA1"
+
+console.log('Hello');
+console.log('First Name: '+ myName);
+console.log('Age: '+myAge);
+console.log('Phone Number: '+myPhoneNumber);
+console.log('Email: '+myEmail);
+console.log('Address:'+myAddress);
+console.log('Program:'+myProgram);
+console.log('IDnum: '+myID);
+console.log('Year: '+ myYear);
+console.log('Section:'+ mySection);
